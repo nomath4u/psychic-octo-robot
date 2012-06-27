94a682d68a011b56a31c918abf14ccab3d53437a
Christopher Harper <nomath4u@gmail.com>
2012-06-27 11:52:38 -0700

    Objectified the running of the game and attempted to improve readability

--- main.cpp
+++ main.cpp
@@ -9,22 +9,19 @@
 #include <iostream>
 #include <cstdlib>
 
-void initialization();
-int generate_target();
-int retrieve_guess(int);
-bool play_again();
-
 
 using namespace std;
+
 
 class Target{
     int val;
     
 public:
+    //Assign target val either random numer or injected number
     Target()
     {
         val = generate_target();
-    
+        
     }
     
     Target(int targetValue)
@@ -32,6 +29,8 @@ public:
         val = targetValue;
     }
     
+    
+    //Generate random number 0 - 100
     int generate_target(){
         srand( (unsigned int)time(NULL) );
         int target = (rand() % 101);
@@ -42,6 +41,9 @@ public:
         cout << val;
     }
     
+    
+    
+    //Compare target val to guess
     bool isSame(int guess)
     {
         
@@ -57,7 +59,100 @@ public:
     {
         return (guess < val);
     }
+    
 };
+
+
+class Guessing_game{
+    const int max_turns = 5;
+    int turn_number;
+    int guess;
+    bool keepPlaying = true;
+    
+public:
+    void play(){
+        while(keepPlaying){
+            
+            //Retrieve guesses until all guesses are used and compare to target
+            Target target;           //Target initialization moved inside of keep playing loop to get a new target every playthrough does old target get destroyed?
+            for(turn_number = 1; turn_number <= max_turns; turn_number++)
+            {
+                guess = retrieve_guess(turn_number);
+                if(target.isSame(guess))
+                {
+                    cout << "You win!" << endl;
+                    break;
+                }
+                
+                else if(target.isHigher(guess))
+                {
+                    cout << "Your guess is too high!" << endl;
+                }
+                
+                else if (target.isLower(guess))
+                {
+                    cout << "Your guess is too low!" << endl;
+                }
+                
+                else
+                {
+                    cout << "Error" << endl;
+                }
+            }
+        cout << "The number was ";
+        target.display();
+        cout << endl << "End Game" << endl;
+        keepPlaying = play_again();
+        }
+    }
+ 
+private:
+    int retrieve_guess(int turn_number){
+        int guess;
+        cout << "Enter your guess. This is turn number " << turn_number<< endl;
+        cin >> guess;
+        return guess;
+        
+    }
+    
+    
+    //Prompt user for playing again
+    bool play_again()
+    {
+        bool choiceFail = true;
+        char selection;
+        bool keepPlaying;
+        while(choiceFail)
+        {
+            cout << "Would you like to continue playing? (y/n)" << endl;
+            cin >> selection;
+            if(selection != 'Y' && selection != 'y' && selection != 'N' && selection != 'n')
+            {
+                cout << "Invalid selection" << endl;
+            }
+            
+            else
+            {
+                choiceFail = false;
+            }
+        }
+        
+        if(selection == 'Y' || selection == 'y')
+        {
+            keepPlaying = true;
+        }
+        
+        else if(selection == 'N' || selection == 'n')
+        {
+            keepPlaying = false;
+        }
+        return keepPlaying;
+    }
+
+};
+
+
+
 
 
 
@@ -66,94 +161,15 @@ int main(int argc, const char * argv[])
 
     // insert code here...
     
-    //initialization
-    
-    //Target target;             Target initialization was here
-    
-    const int max_turns = 5;
-    int turn_number;
-    int guess;
-    bool keepPlaying = true;
-    
-    while(keepPlaying){
-    
-        //Retrieve guesses until all guesses are used and compare to target
-        Target target;           //Target initialization moved inside of keep playing loop to get a new target every playthrough does old target get destroyed?
-        for(turn_number = 1; turn_number <= max_turns; turn_number++)
-        {
-            guess = retrieve_guess(turn_number);
-            if(target.isSame(guess))
-            {
-                cout << "You win!" << endl;
-                break;
-            }
-        
-            else if(target.isHigher(guess))
-            {
-                cout << "Your guess is too high!" << endl;
-            }
-        
-            else if (target.isLower(guess))
-            {
-                cout << "Your guess is too low!" << endl;
-            }
-            
-            else
-            {
-                cout << "Error" << endl;
-            }
-        }
-    cout << "The number was ";
-    target.display();
-    cout << endl << "End Game" << endl;
-    keepPlaying = play_again();
-    }
+
+    Guessing_game game;
+    game.play();    //Start the game
     return 0;
 }
 
 
 
 
-int retrieve_guess(int turn_number){
-    int guess;
-    cout << "Enter your guess. This is turn number " << turn_number<< endl;
-    cin >> guess;
-    return guess;
-    
-}
-
-bool play_again()
-{
-    bool choiceFail = true;
-    char selection;
-    bool keepPlaying;
-    while(choiceFail)
-    {
-        cout << "Would you like to continue playing? (y/n)" << endl;
-        cin >> selection;
-        if(selection != 'Y' && selection != 'y' && selection != 'N' && selection != 'n')
-        {
-        cout << "Invalid selection" << endl;
-        }
-        
-        else
-        {
-            choiceFail = false;
-        }
-    }
-       
-    if(selection == 'Y' || selection == 'y')
-    {
-        keepPlaying = true;
-    }
-    
-    else if(selection == 'N' || selection == 'n')
-    {
-        keepPlaying = false;
-    }
-    return keepPlaying;
-}
-       
        
        
        
